fe0ddb23ec8409c3eec6db5226fc0880e80a4b84
Alvaro <alvaritodavilag0331@gmail.com>
2024-03-31 23:59:15 -0600

    Funcion que limpia display y variables

--- Calculadora.js
+++ Calculadora.js
@@ -13,6 +13,17 @@ function setOperador(operador) {
     Operador1 = document.getElementById('Display').value;
     ActualOperador = operador;
     console.log(ActualOperador);
+    console.log(Operador1);
     //Limpiar display
     document.getElementById('Display').value = '';
+}
+
+function LimpiarDisplay() {
+    Operador1 = '';
+    Operador2 = '';
+    ActualOperador = '';
+    document.getElementById('Display').value = '';
+    console.log(ActualOperador);
+    console.log(Operador1);
+    console.log(Operador2);
 }
