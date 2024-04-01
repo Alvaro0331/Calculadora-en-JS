b1fcbf404a93ecec8d721b27f12a3a877f6d5ad5
Alvaro <alvaritodavilag0331@gmail.com>
2024-03-31 23:53:35 -0600

    Creada funcion que limpia display y variables

--- Calculadora.js
+++ Calculadora.js
@@ -1,6 +1,18 @@
+let Operador1 = '';
+let Operador2 = '';
+let ActualOperador = '';
+
 function Concatenar(id) {
     //Extraer numero
     var Numero = document.getElementById(id).innerHTML;
     //Mostrar en el display
     document.getElementById('Display').value += Numero;
+}
+
+function setOperador(operador) {
+    Operador1 = document.getElementById('Display').value;
+    ActualOperador = operador;
+    console.log(ActualOperador);
+    //Limpiar display
+    document.getElementById('Display').value = '';
 }
